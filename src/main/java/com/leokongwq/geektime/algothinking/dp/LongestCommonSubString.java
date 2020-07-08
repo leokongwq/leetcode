dafd2c100f07ad90de71067d951828afabc51db9
kongwenqiang <kongwenqiang@qiyi.com>
2020-07-08 09:06:26 +0800

    刷题提交

--- src/main/java/com/leokongwq/geektime/algothinking/dp/LongestCommonSubString.java
+++ src/main/java/com/leokongwq/geektime/algothinking/dp/LongestCommonSubString.java
@@ -9,61 +9,46 @@ package com.leokongwq.geektime.algothinking.dp;
  **/
 public class LongestCommonSubString {
 
-	private int maxLen = 0;
-
-	public void max_lcs(String a, String b) {
-		if (a.length() < b.length()) {
-			max_lcs(a.toCharArray(), 0, b.toCharArray(), 0, 0);
-		} else {
-			max_lcs(b.toCharArray(), 0, a.toCharArray(), 0, 0);
-		}
-	}
-
-	private void max_lcs(char[] a, int i, char[] b, int j, int curLen) {
-		if (i == a.length || j == b.length) {
-			if (curLen > maxLen) {
-				maxLen = curLen;
-			}
-			return;
-		}
-		//
-		if (a[i] == b[j]) {
-			max_lcs(a, i + 1, b, j + 1, curLen + 1);
-		} else {
-			// 删除 a[i]，或者在 b[j]前面加上一个字符 a[i]，然后继续考察 a[i+1]和 b[j]；
-			max_lcs(a, i + 1, b, j, curLen);
-			// 删除 b[j]，或者在 a[i]前面加上一个字符 b[j]，然后继续考察 a[i]和 b[j+1]。
-			max_lcs(a, i, b, j + 1, curLen);
-		}
-	}
-
 	/**
-	 * 暴力解法
+	 * 暴力解法 - 倒序遍历
 	 * 可以通过备忘录来避免重复计算
 	 */
-	private static int max_lcs(char[] a, int i, char[] b, int j) {
+	int max_lcsV1(char[] a, int i, char[] b, int j) {
 		if (i == -1 || j == -1) {
 			return 0;
 		}
 		if (a[i] == b[j]) {
-			return max_lcs(a, i - 1, b, j - 1) + 1;
+			return max_lcsV1(a, i - 1, b, j - 1) + 1;
 		} else {
-			return Math.max(max_lcs(a, i - 1, b, j), max_lcs(a, i, b, j - 1));
+			return Math.max(max_lcsV1(a, i - 1, b, j), max_lcsV1(a, i, b, j - 1));
 		}
 	}
 
-	private int max(int x, int y, int z) {
-		int maxv = Integer.MIN_VALUE;
-		if (x > maxv) {
-			maxv = x;
+    /**
+     * 暴力解法 - 正序比较
+     */
+	int max_lcsV2(String a, String b) {
+		if (a.length() < b.length()) {
+			return max_lcs(a.toCharArray(), 0, b.toCharArray(), 0);
+		} else {
+			return max_lcs(b.toCharArray(), 0, a.toCharArray(), 0);
 		}
-		if (y > maxv) {
-			maxv = y;
+	}
+
+	private int max_lcs(char[] a, int i, char[] b, int j) {
+		if (i == a.length || j == b.length) {
+			return 0;
 		}
-		if (z > maxv) {
-			maxv = z;
+		//
+		if (a[i] == b[j]) {
+			return max_lcs(a, i + 1, b, j + 1) + 1;
+		} else {
+			// 删除 a[i]，或者在 b[j]前面加上一个字符 a[i]，然后继续考察 a[i+1]和 b[j]；
+			int max1 = max_lcs(a, i + 1, b, j);
+			// 删除 b[j]，或者在 a[i]前面加上一个字符 b[j]，然后继续考察 a[i]和 b[j+1]。
+			int max2 = max_lcs(a, i, b, j + 1);
+			return Math.max(max1, max2);
 		}
-		return maxv;
 	}
 
 	public static void main(String[] args) {
@@ -74,8 +59,10 @@ public class LongestCommonSubString {
 //		String a = "befdc";
 		String a = "bbdc";
 		String b = "bc";
-		css.max_lcs(a, b);
-		System.out.println(css.maxLen);
-		System.out.println(max_lcs(a.toCharArray(), a.length() - 1, b.toCharArray(), b.length() - 1));
+
+		int lcs = css.max_lcsV1(a.toCharArray(), a.length() - 1, b.toCharArray(), b.length() - 1);
+        System.out.println(lcs);
+
+		System.out.println(css.max_lcsV2(a, b));
 	}
 }
